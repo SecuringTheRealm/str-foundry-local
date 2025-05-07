193a85e62a9f40acaed989417df96db7b81804ce
Chris Lloyd-Jones <19361656+Sealjay@users.noreply.github.com>
2025-05-07 10:53:37 -0700

    Fixes whitespace above the page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Chat from '@/components/Chat';
+import Chat from "@/components/Chat";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen h-screen flex-col items-center justify-between p-6 md:p-12 lg:p-24 bg-[#F7F7F7]">
+    <main className="flex min-h-screen h-screen flex-col items-center justify-between p-6 md:p-12 bg-[#F7F7F7]">
       <div className="w-full flex-1">
         <Chat />
       </div>
